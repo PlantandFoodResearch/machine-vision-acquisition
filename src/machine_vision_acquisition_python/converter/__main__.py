d8c1b2b5417951c8bb06581308badf961dd6547f
Manu Lange <manu.lange@plantandfood.co.nz>
2022-06-17 16:22:55 +1200

    some initial stuff

--- src/machine_vision_acquisition_python/converter/__main__.py
+++ src/machine_vision_acquisition_python/converter/__main__.py
@@ -4,8 +4,10 @@ import typing
 import datetime
 import cv2
 import logging
+import json
 import multiprocessing
 from machine_vision_acquisition_python.converter.processing import cvt_tonemap_image
+from machine_vision_acquisition_python.utils import get_image_mean, get_image_sharpness, get_image_std
 
 
 logging.basicConfig(level=logging.DEBUG)
@@ -101,6 +103,57 @@ def process_file(in_path: Path, out_dir: Path, tonemap: bool):
         image = cvt_tonemap_image(image)
     if not cv2.imwrite(str(out_path), image):
         raise ValueError(f"Failed to write {out_path.name}")
+
+
+def process_folder_stats(input_path: Path, output_path: Path):
+    process_args = []
+    result_queue: multiprocessing.Queue[typing.Dict] = multiprocessing.Queue()
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    # Gather work
+    for file_path in input_path.rglob("*.png"):
+        process_args.append((file_path.resolve(), result_queue))
+
+    # Multiprocess
+    pool = multiprocessing.Pool(processes=8)
+    try:
+        log.info("Processing {} files in {}".format(len(process_args), str(input_path)))
+        pool.starmap(process_file_stats, process_args)
+        log.info("Done :)")
+        # Gather results
+        resuls = gather_results(result_queue)
+        output_path.write_text(json.dumps(resuls))
+    except KeyboardInterrupt as _:
+        log.warning("Aborting processing")
+    finally:
+        pool.close()
+        pool.terminate()
+        pool.join()
+
+
+def process_file_stats(in_path: Path, result_queue: multiprocessing.Queue[typing.Dict] ):
+    """
+    Multoprocessing entrypoint for doing the grunt work
+    """
+    log.debug(f"Processing {in_path}")
+    image = cv2.imread(str(in_path), cv2.IMREAD_ANYDEPTH)
+    sharpness = get_image_sharpness(image)
+    mean = get_image_mean(image)
+    std = get_image_std(image)
+    outputs = {
+        "file_name": str(in_path.name),
+        "sharpness": sharpness,
+        "mean": mean,
+        "std": std
+    }
+    result_queue.put(outputs)
+
+
+def gather_results(result_queue: multiprocessing.Queue[typing.Dict]):
+    results = []
+    while not result_queue.empty():
+        entry = result_queue.get_nowait()
+        results.append(entry)
+    return results
 
 
 if __name__ == "__main__":
