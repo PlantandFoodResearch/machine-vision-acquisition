832ee44f2f207505bdf86af2ded5e487938476ab
Manu Lange <manu.lange@plantandfood.co.nz>
2022-06-22 07:02:14 +1200

    add multiprocessing

--- src/machine_vision_acquisition_python/process/commands/undistort.py
+++ src/machine_vision_acquisition_python/process/commands/undistort.py
@@ -79,13 +79,36 @@ def undistort(
     # Todo: fix this somehow camera serial mappings
     calibration = calibrationts[0]
 
+    process_args = []
     undistorter = Undistorter(calibration)
     for file_path in input_path.rglob("*.png"):
-        image = cv2.imread(str(file_path))
         if not undistorter.initialised:
+            image = cv2.imread(str(file_path))
             undistorter.init_optimal_matrix(image.shape)
-        undistored = undistorter.undistort(image)
-        output_file_path = output_path / f"{file_path.stem}-undistorted.png"
-        if not cv2.imwrite(str(output_file_path), undistored):
-            raise ValueError(f"Failed to write {output_file_path.name}")
-        log.info(f"Undistorted {file_path.name}")
+        out_dir = output_path / file_path.parent.relative_to(input_path)
+        out_dir.mkdir(exist_ok=True, parents=True)
+        process_args.append((file_path.resolve(), out_dir.resolve(), undistorter))
+
+    pool = multiprocessing.Pool(processes=1)
+    try:
+        log.info("Processing {} files in {}".format(len(process_args), str(input_path)))
+        pool.starmap(process_file, process_args)
+        log.info("Done :)")
+    except KeyboardInterrupt as _:
+        log.warning("Aborting processing")
+    finally:
+        pool.close()
+        pool.terminate()
+        pool.join()
+
+
+def process_file(in_path: Path, out_dir: Path, undistorter: Undistorter):
+    """Undistort single file for multiprocessing"""
+    image = cv2.imread(str(in_path))
+    if not undistorter.initialised:
+        undistorter.init_optimal_matrix(image.shape)
+    undistored = undistorter.undistort(image)
+    output_file_path = out_dir / f"{in_path.stem}-undistorted.{in_path.suffix}"
+    if not cv2.imwrite(str(output_file_path), undistored):
+        raise ValueError(f"Failed to write {output_file_path.name}")
+    log.info(f"Undistorted {in_path.name}")
