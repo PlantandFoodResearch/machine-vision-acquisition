b757edf2faf1f2eee9a8f6248d7a47a8498542eb
Manu Lange <manu.lange@plantandfood.co.nz>
2022-07-14 13:52:37 +1200

    fixed stereo bm engine

--- src/machine_vision_acquisition_python/calibration/stereo.py
+++ src/machine_vision_acquisition_python/calibration/stereo.py
@@ -48,25 +48,50 @@ if __name__ == "__main__":
     np.put(t2, [0,1,2,4,5,6,8,9,10], r2[0])
 
     # calculate cam2 pose realtive to cam1
-    t=np.matmul(t2, np.linalg.inv(t1))
-    R=cv2.Rodrigues(t[0:3, 0:3])
-    R=R[0]
-    T=t[0:3, 3]
+    t=np.matmul(np.linalg.inv(t1), t2)
+    #R=cv2.Rodrigues(t[0:3, 0:3])
+    #R=R[0]
+    #R=t[0:3, 0:3]
+    #T=t[0:3, 3]
 
+    #https://answers.opencv.org/question/89968/how-to-derive-relative-r-and-t-from-camera-extrinsics/
+    R=np.matmul(np.linalg.inv(r1[0]), r2[0])
+    T=np.matmul(r1[0].T, calibrations[2].tvec.T) - np.matmul(r1[0].T, calibrations[1].tvec.T)
     imageSize=[1920, 1080]
     R1 = np.zeros(shape=(3,3))
     R2 = np.zeros(shape=(3,3))
-    P1 = np.zeros(shape=(3,3))
-    P2 = np.zeros(shape=(3,3))
-    cv2.stereoRectify(calibrations[1].cameraMatrix, calibrations[1].distCoeffs, calibrations[2].cameraMatrix, calibrations[2].distCoeffs, imageSize, R, T, R1, R2, P1, P2, flags=cv2.CALIB_ZERO_DISPARITY)
+    P1 = np.zeros(shape=(3,4))
+    P2 = np.zeros(shape=(3,4))
+    Q = np.zeros(shape=(4,4))
 
-    mapLeftX, mapLeftY = cv2.initUndistortRectifyMap(calibrations[1].cameraMatrix, calibrations[1].distCoeffs, R1, calibrations[1].cameraMatrix,imageSize, cv2.CV_32FC1)
-    mapRightX, mapRightY = cv2.initUndistortRectifyMap(calibrations[2].cameraMatrix, calibrations[2].distCoeffs, R2, calibrations[2].cameraMatrix, imageSize, cv2.CV_32FC1)
+   
+    cv2.stereoRectify(calibrations[1].cameraMatrix, calibrations[1].distCoeffs, calibrations[2].cameraMatrix, calibrations[2].distCoeffs, imageSize, cv2.Rodrigues(R)[0], T, R1, R2, P1, P2, Q, flags=0, alpha=-1)
 
-    leftImage = cv2.remap(sourceLeft, mapLeftY, mapLeftY)
-    rightImage = cv2.remap(sourceRight, mapRightX, mapRightY)
+    mapLeftX, mapLeftY = cv2.initUndistortRectifyMap(calibrations[1].cameraMatrix, calibrations[1].distCoeffs, R1, P1,imageSize, cv2.CV_16SC2)
+    mapRightX, mapRightY = cv2.initUndistortRectifyMap(calibrations[2].cameraMatrix, calibrations[2].distCoeffs, R2, P2, imageSize, cv2.CV_16SC2)
 
-    blockMatcher = cv2.StereoBM_create(32, 16)
+    sourceLeft = cv2.imread('/home/user/workspace/cfnmxl/ssis_orchard_imaging/tmp/Lucid_213500023-20220708-102603-tonemapped/img00013_exp1000_2022-07-07_14-50-59-935.tonemapped.png')
+    sourceRight = cv2.imread('/home/user/workspace/cfnmxl/ssis_orchard_imaging/tmp/Lucid_213500031-20220708-102957-tonemapped/img00013_exp1000_2022-07-07_14-50-59-717.tonemapped.png')
+    cv2.imwrite('./tmp/sourceLeft.png', sourceLeft)
+    cv2.imwrite('./tmp/sourceRight.png', sourceRight)
 
+    # stereo blockmatcher works in single channel image
+    sourceLeft = cv2.cvtColor(sourceLeft, cv2.COLOR_BGR2GRAY)
+    sourceRight = cv2.cvtColor(sourceRight, cv2.COLOR_BGR2GRAY)
+    leftImage = cv2.remap(sourceLeft, mapLeftX, mapLeftY, cv2.INTER_LINEAR)
+    rightImage = cv2.remap(sourceRight, mapRightX, mapRightY, cv2.INTER_LINEAR)
 
-    test = BlockMatcher(None)
+    
+    cv2.imwrite('./tmp/leftRect.png', leftImage)
+    cv2.imwrite('./tmp/rightRect.png', rightImage)    
+
+    stereo = cv2.StereoBM_create(128, 27)
+    stereo.setMinDisparity(0)
+    disparity = stereo.compute(leftImage, rightImage).astype(float) / 16.
+    
+    disp8bit=cv2.convertScaleAbs(disparity)
+    cv2.imwrite('./tmp/disp.png', disp8bit)
+ 
+    xyz=cv2.reprojectImageTo3D(disparity, Q)
+    
+    
